234dc3a39c199bcce998547aa8b50d1620b77b91
ahhe1007 <119276968+ahhe1007@users.noreply.github.com>
2022-11-29 00:03:39 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,4 @@ bot = commands.bot(command_prefix= '!')
 async def on_ready():
     print(">> Bot is online ")
 
-bot.run('MTA0NjYzMTk1OTA2NzExOTc2OA.GhPzTo.lGxXt3d6LyquCe1c2N_XH6Qps9POSR2NGdwIts')
+bot.run('')
